34d66612ad5958eaf55c300d7679709de5fcfd1d
Dylan Wight <dylangwight@gmail.com>
2016-10-10 20:27:46 -0400

    createCell

---
+++ MessagesExtension/CreateCell.swift
@@ -0,0 +1,16 @@
+//
+//  CreateCell.swift
+//  StickPics
+//
+//  Created by Dylan Wight on 10/10/16.
+//  Copyright © 2016 Dylan Wight. All rights reserved.
+//
+
+import UIKit
+
+class CreateCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "CreateCell"
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- MessagesExtension/StickPicsCollectionViewController.swift
+++ MessagesExtension/StickPicsCollectionViewController.swift
@@ -12,28 +12,47 @@ import Messages
 
 class StickPicsCollectionViewController: UICollectionViewController {
     
+    enum Section: Int {
+        case create = 0
+        case stickPic = 1
+    }
+    
     // MARK: Properties
     
     static let storyboardIdentifier = "StickPicsCollectionViewController"
     
     // MARK: UICollectionViewDataSource
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return StickPicHistory.load().stickPicURLs.count
+        switch Section(rawValue: section)! {
+        case .create:
+            return 1
+        case .stickPic:
+            return StickPicHistory.load().stickPicURLs.count
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let url = StickPicHistory.load().stickPicURLs[indexPath.row]
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StickPicCell", for: indexPath) as! StickPicCell
-        
-        do {
-            cell.stickerView.sticker = try MSSticker(contentsOfFileURL: url, localizedDescription: "test")
-        } catch {
-            fatalError("Failed to load sticker image: \(error)")
+        switch Section(rawValue: indexPath.section)! {
+        case .create:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CreateCell.reuseIdentifier, for: indexPath) as! CreateCell
+            return cell
+        case .stickPic:
+            let url = StickPicHistory.load().stickPicURLs[indexPath.row]
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StickPicCell.reuseIdentifier, for: indexPath) as! StickPicCell
+            
+            do {
+                cell.stickerView.sticker = try MSSticker(contentsOfFileURL: url, localizedDescription: "test")
+            } catch {
+                fatalError("Failed to load sticker image: \(error)")
+            }
+            
+            return cell
         }
-        
-        return cell
     }
 }
 
